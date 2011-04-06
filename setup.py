272a712bf62c0eddb6396b03f2f050929bea1c68
Ian McKellar <ian@mckellar.org>
2011-04-05 20:23:51 -0700

    rename 'rdio' package to 'rdioapi', release version 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Rdio',
-      version='0.1',
+      version='0.2',
       description='A Python wrapper library for the Rdio web services API',
       long_description='''A wrapper for Rdio's web service API.
 This includes versions of the httplib2 and oauth2 libraries that work well with the Rdio service.
