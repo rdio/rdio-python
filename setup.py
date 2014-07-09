7d7437da24120046ae79448af2efdbcd33709f01
Devin Sevilla <devin.sevilla@rd.io>
2014-07-09 13:39:57 -0700

    Improve packaging
    - Depend on oauth2 instead of including it
    - Improve setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,32 @@
 
 from setuptools import setup, find_packages
 
-setup(name='Rdio',
-      version='0.2',
-      description='A Python wrapper library for the Rdio web services API',
-      long_description='''A wrapper for Rdio's web service API.
-This includes versions of the httplib2 and oauth2 libraries that work well with the Rdio service.
-This also includes a command-line tool rdio-call for making API calls.''',
-      author='Rdio',
-      author_email='developersupport@rd.io',
-      platforms='any',
-      license='MIT',
-      url='http://www.rdio.com/developers/',
-      packages=find_packages(),
-      scripts=['rdio-call'],
-)
+long_description = '''A wrapper for Rdio's web service API.
 
+This also includes a command-line tool rdio-call for making API calls.'''
+
+setup(
+    name='Rdio',
+    version='0.3.0',
+
+    description='A Python wrapper library for the Rdio web services API',
+    long_description=long_description,
+
+    author='Rdio',
+    author_email='developersupport@rd.io',
+
+    url='http://www.rdio.com/developers/',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    license='MIT',
+    platforms='any',
+
+    install_requires=['oauth2'],
+
+    packages=find_packages(),
+
+    scripts=['rdio-call'],
+)
